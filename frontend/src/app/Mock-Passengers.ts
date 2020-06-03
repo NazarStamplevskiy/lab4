5ea514f981d6c9f29b0129a22238c76e4f9b327a
NazarStamplevskiy <Nazzarstamplevskii@gmail.com>
2020-06-03 12:15:27 +0300

    Add lab4

---
+++ frontend/src/app/Mock-Passengers.ts
@@ -0,0 +1,9 @@
+import { Passenger } from './Passenger';
+
+export const PASSENGERS: Passenger[] = [
+    {id: 1, name: 'Yurii', surname: 'Pasternak'},
+    {id: 2, name: 'Oleh', surname: 'Kozak'},
+    {id: 3, name: 'Max', surname: 'Hetmanchuk'},
+    {id: 4, name: 'Yulia', surname: 'Pushka'},
+    {id: 5, name: 'Natalia', surname: 'Holubinka'},
+]
